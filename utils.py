4b26fd582eae1f910ce42fde58dedba598225240
toilaluan <toilaluan@github.com>
2023-12-24 20:43:30 +0700

    update docker compose

--- utils.py
+++ utils.py
@@ -2,6 +2,36 @@ import base64
 import requests
 from PIL import Image
 from io import BytesIO
+import json
+import base64
+from PIL import Image
+import io
+import matplotlib.pyplot as plt
+
+# Function to decode and display an image
+def display_image(encoded_image, title, price):
+    # Decode the image
+    price = price.replace("$","")
+    image_data = base64.b64decode(encoded_image)
+    image = Image.open(io.BytesIO(image_data))
+    image.save(f"weaviate/tmp/{title}-{price}.png")
+    # # Display the image
+    plt.imshow(image)
+    plt.title(f"{title} - ${price}")
+    plt.axis('off')
+    plt.show()
+
+# Function to process and visualize the JSON file
+def visualize_json(file_path):
+    # Read the JSON file
+    with open(file_path, 'r') as file:
+        data = json.load(file)['data']['Get']['Estate']
+
+    # Iterate over each item and display the image
+    for item in data:
+        # print(item)
+        display_image(item['image'], item['title'], item['price'])
+
 
 
 def load_and_convert_image(url):
